7707a22fc8254357a3bcdd6387591fb6289769ea
haitham2234 <128534861+haitham2234@users.noreply.github.com>
2023-04-12 08:24:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace analyzemepro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
